bb201c5fa0719e4104fc52533398cc4621a3a954
Otávio de Castro <otavio.vn01@hotmail.com>
2020-01-17 15:08:14 -0200

    Estado e cidade

--- src/main/java/com/init0/cursomc/CursomcApplication.java
+++ src/main/java/com/init0/cursomc/CursomcApplication.java
@@ -8,18 +8,28 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.init0.cursomc.domain.Categoria;
+import com.init0.cursomc.domain.Cidade;
+import com.init0.cursomc.domain.Estado;
 import com.init0.cursomc.domain.Produto;
 import com.init0.cursomc.repositories.CategoriaRepository;
+import com.init0.cursomc.repositories.CidadeRepository;
+import com.init0.cursomc.repositories.EstadoRepository;
 import com.init0.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
-	CategoriaRepository categoriaRepository;
+	private CategoriaRepository categoriaRepository;
 	
 	@Autowired
-	ProdutoRepository produtoRepository;
+	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -28,9 +38,11 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
+		//Populando a tabela Categoria
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+
+		//Populando a tabela Produto
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
@@ -50,6 +62,20 @@ public class CursomcApplication implements CommandLineRunner {
 		//Salvando os objetos de produto no BD
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
+		
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		
 	}
 
