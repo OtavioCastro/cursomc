0876640662e44972bb4c9bfc0b956991bb04ed58
Otávio de Castro <otavio.vn01@hotmail.com>
2020-01-17 17:02:52 -0200

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/init0/cursomc/CursomcApplication.java
+++ src/main/java/com/init0/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.init0.cursomc.domain.Categoria;
 import com.init0.cursomc.domain.Cidade;
+import com.init0.cursomc.domain.Cliente;
+import com.init0.cursomc.domain.Endereco;
 import com.init0.cursomc.domain.Estado;
 import com.init0.cursomc.domain.Produto;
+import com.init0.cursomc.domain.enums.TipoCliente;
 import com.init0.cursomc.repositories.CategoriaRepository;
 import com.init0.cursomc.repositories.CidadeRepository;
+import com.init0.cursomc.repositories.ClienteRepository;
+import com.init0.cursomc.repositories.EnderecoRepository;
 import com.init0.cursomc.repositories.EstadoRepository;
 import com.init0.cursomc.repositories.ProdutoRepository;
 
@@ -30,6 +35,15 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private EstadoRepository estadoRepository;
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+
+	
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -63,19 +77,41 @@ public class CursomcApplication implements CommandLineRunner {
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
 		
-		
+		//Populando a tabela Estado
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
 		
+		//Populando a tabela Cidade
 		Cidade c1 = new Cidade(null, "Uberlândia", est1);
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
 		
+		//Adicionando a lista de cidades aos seus respectivos estados
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2,c3));
 		
+		//Salvando os objetos de estado no BD
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		
+		//Salvando os objetos de cidade no BD
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		
+		//Populando a tabela Cliente
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
+
+		//Populando a tabela Endereco		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		//Adicionando a lista de endereços ao cliente
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		//Salvando os objetos de cliente no BD
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		
+		//Salvando os objetos de endereco no BD
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
 		
 	}
 
