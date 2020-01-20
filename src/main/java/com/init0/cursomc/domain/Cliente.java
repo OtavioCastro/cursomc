02b29981d929e776db2278834b787610019fb0ae
Otávio de Castro <otavio.vn01@hotmail.com>
2020-01-20 15:51:11 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/init0/cursomc/domain/Cliente.java
+++ src/main/java/com/init0/cursomc/domain/Cliente.java
@@ -14,8 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.init0.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -30,16 +29,14 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;	
 	private Integer tipo;
 
-	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
-	@JsonManagedReference
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToMany(mappedBy="cliente")	
 	private List<Pedido> pedidos = new ArrayList<>();
 
